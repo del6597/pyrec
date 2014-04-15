00ccfd945133094c71151cd8d6359a0a73570543
David Lamont <del6597@rit.edu>
2014-04-15 17:16:49 -0400

    Config file

--- pyre.py
+++ pyre.py
@@ -1,7 +1,11 @@
+from configparser import SafeConfigParser
+
 class Pyre:
     def __init__(self):
         pass
     
 def main():
+    parser = SafeConfigParser()
+    parser.read('config.ini')
     bot = new Pyre()
 main()
